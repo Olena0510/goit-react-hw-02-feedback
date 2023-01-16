e77dfd1ada3d14bdd81abd0794a11a7e6ba6cf7c
Olena Kameneva <lenakr0910@gmail.com>
2023-01-16 19:08:05 +0200

    feedb

--- src/components/FeedbackOptions/Feedbackoptions.jsx
+++ src/components/FeedbackOptions/Feedbackoptions.jsx
@@ -1,18 +1,24 @@
 import PropTypes from 'prop-types';
 
-export FeedbackOptions = ({ options, onLeaveFeedback }) => {
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
         <div>
-            {options.map(option)} => (
-            (<button key={option} type='button' onClick={() => onLeaveFeedback(option)}>{option}</button>
-            )
-            );
+           {options.map(option => {
+        return (
+          <button
+            type="button"
+            key={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        );
+      })}
         </div>
     );
 
-
+}
     FeedbackOptions.propTypes = {
-        options: PropTypes.arrayOf(PropTypes.string).isRequired,
-        onLeaveFeedback: PropTypes.func.isRequired,
+        options: PropTypes.arrayOf(PropTypes.string.isRequired),
+            onLeaveFeedback: PropTypes.func.isRequired,
     };
-};
