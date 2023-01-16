c4e70f79b4e77be561271f40f30723a438edd86e
Olena Kameneva <lenakr0910@gmail.com>
2023-01-16 18:54:43 +0200

    feed

--- src/components/FeedbackOptions/Feedbackoptions.jsx
+++ src/components/FeedbackOptions/Feedbackoptions.jsx
@@ -6,6 +6,7 @@ export FeedbackOptions = ({ options, onLeaveFeedback }) => {
             {options.map(option)} => (
             (<button key={option} type='button' onClick={() => onLeaveFeedback(option)}>{option}</button>
             )
+            );
         </div>
     );
 
