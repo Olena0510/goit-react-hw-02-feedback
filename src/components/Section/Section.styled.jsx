c92d0d7cc4b8facedb0820d126c07726c0ae490c
Olena Kameneva <lenakr0910@gmail.com>
2023-01-17 16:02:32 +0200

    feedff

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,30 +1,16 @@
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 
-
-// export const Wrapper = styled.section`
-//  padding-top: 24px;
-//   padding-right: 15px;
-//   padding-left: 15px;
-//   padding-bottom: 24px;
-//   text-align: center;
-// `;
-
-// export const WrapperTitle = styled.h2`
-//   font-size: 36px;
-//   margin-bottom: 30px;
-// `;
-
-import styled from '@emotion/styled';
 
 export const Wrapper = styled.section`
-  padding-top: 24px;
+ padding-top: 24px;
   padding-right: 15px;
   padding-left: 15px;
   padding-bottom: 24px;
   text-align: center;
 `;
 
-export const Title = styled.h2`
-  font-size: 48px;
+export const WrapperTitle = styled.h2`
+  font-size: 36px;
   margin-bottom: 30px;
 `;
+
